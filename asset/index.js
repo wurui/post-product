0375f9e8a7a44842a8faf0b4fae6e30ee4ec977d
wurui <wurui.wr@gmail.com>
2017-10-03 19:47:37 +0800

    REST

--- asset/index.js
+++ asset/index.js
@@ -1,27 +1,25 @@
-define(['oxjs'], function (OXJS) {
+define(['oxjs'], function(OXJS) {
     return {
-        init: function ($mod) {
-            var dsid = $mod.attr('data-dsid'),
-                uid=$mod.attr('data-uid');
-            var forward=$mod.attr('data-forward');
-            var productRest=OXJS.useREST('product/'+dsid+'/u/' + encodeURIComponent(uid)).setDevHost('http://local.openxsl.com/');//md5('saomachetie')
-            $('.J_mainform',$mod).on('submit',function(e){
-                var data=OXJS.formToJSON(e.target);
-                if(data._id){
-                    productRest.put(data,function(r){
-                        if(r.code==0){
+        init: function($mod) {
+            var forward = $mod.attr('data-forward');
+            var productRest = OXJS.useREST('product').setDevHost('http://dev.openxsl.com/'); //md5('saomachetie')
+            $('.J_mainform', $mod).on('submit', function(e) {
+                var data = OXJS.formToJSON(e.target);
+                if (data._id) {
+                    productRest.put(data, function(r) {
+                        if (r.code == 0) {
                             //console.log('gggood!')
-                            location.href=forward;
-                        }else{
+                            location.href = forward;
+                        } else {
                             OXJS.toast(r.message)
                         }
                     });
-                }else{
-                    productRest.post(data,function(r){
-                        if(r.code==0){
+                } else {
+                    productRest.post(data, function(r) {
+                        if (r.code == 0) {
                             //console.log('gggood!')
-                            location.href=forward;
-                        }else{
+                            location.href = forward;
+                        } else {
                             OXJS.toast(r.message)
                         }
                     });
--- mod.xsl
+++ mod.xsl
@@ -1,10 +1,8 @@
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:oxm="https://www.openxsl.com">
     <xsl:template match="/root" name="wurui.post-product">
-        <xsl:param name="dsid">e0ee59439b39fcc3</xsl:param>
         <xsl:param name="forward"></xsl:param>
         <!-- className 'J_OXMod' required  -->
-        <div class="J_OXMod oxmod-post-product" ox-mod="post-product" data-forward="{$forward}" data-dsid="{$dsid}"
-             data-uid="{login/uid}">
+        <div class="J_OXMod oxmod-post-product" ox-mod="post-product" data-forward="{$forward}" data-uid="{login/uid}">
             <xsl:variable name="product" select="data/product"/>
             <form class="J_mainform">
                 <xsl:if test="$product/_id !=''">
